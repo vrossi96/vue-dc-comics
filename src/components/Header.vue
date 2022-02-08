4dce364739d5bf0230a38ca3601d046112e159dd
vrossi96 <vrossi396@yahoo.it>
2022-02-08 14:43:13 +0100

    add initial header

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+   <header>Header</header>
+</template>
+
+<script>
+export default {
+   name: "Header",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+   font-size: 50px;
+}
+</style>
