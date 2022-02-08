0644bf25f514ab98c2f77136957defa0307f7976
vrossi96 <vrossi396@yahoo.it>
2022-02-08 18:37:19 +0100

    added footer with links, images doesnt load

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,7 +1,15 @@
 <template>
    <div id="bottom-footer">
       <div class="container">
-         <h2>Content Goes Here</h2>
+         <div>
+            <a class="sign-up" href="#">SIGN-UP NOW!</a>
+         </div>
+         <div class="social">
+            <h2>FOLLOW US</h2>
+            <a v-for="logo in social" :key="logo.name" href="#">
+               <img :src="`../assets/img/${logo.img}.png`" :alt="logo.name" />
+            </a>
+         </div>
       </div>
    </div>
 </template>
@@ -9,6 +17,17 @@
 <script>
 export default {
    name: "BottomFooter",
+   data() {
+      return {
+         social: [
+            { name: "facebook", img: "facebook" },
+            { name: "periscope", img: "periscope" },
+            { name: "pinterest", img: "pinterest" },
+            { name: "twitter", img: "twitter" },
+            { name: "youtube", img: "youtube" },
+         ],
+      };
+   },
 };
 </script>
 
@@ -20,7 +39,28 @@ export default {
 
    .container {
       display: flex;
+      justify-content: space-between;
       align-items: center;
+
+      .social {
+         display: flex;
+         justify-content: space-between;
+         align-items: center;
+      }
+   }
+
+   .sign-up {
+      text-decoration: none;
+      font-size: 25px;
+      padding: 10px 20px;
+      border: 2px solid #0282f9;
+      color: white;
+   }
+
+   h2 {
+      font-weight: 500;
+      color: #0282f9;
+      padding-right: 15px;
    }
 }
 </style>
