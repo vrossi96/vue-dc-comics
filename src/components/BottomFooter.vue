68ee16db7ce16b9a4f6f446e8a31e3615425cb3a
vrossi96 <vrossi396@yahoo.it>
2022-02-09 12:59:18 +0100

    fixed bottom footer v-for socials

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -6,20 +6,8 @@
          </div>
          <div class="social">
             <h2>FOLLOW US</h2>
-            <a href="#">
-               <img src="../assets/img/footer-facebook.png" alt="facebook" />
-            </a>
-            <a href="#">
-               <img src="../assets/img/footer-periscope.png" alt="periscope" />
-            </a>
-            <a href="#">
-               <img src="../assets/img/footer-pinterest.png" alt="pinterest" />
-            </a>
-            <a href="#">
-               <img src="../assets/img/footer-twitter.png" alt="twitter" />
-            </a>
-            <a href="#">
-               <img src="../assets/img/footer-youtube.png" alt="youtube" />
+            <a v-for="(icon, index) in social" :key="index" href="#">
+               <img :src="require(`../assets/img/footer-${icon.img}.png`)" :alt="icon.name" />
             </a>
          </div>
       </div>
